0cff0d650f316afa6e86181fdf132fd39fbea522
Huw Fryer <huwfryer@gmail.com>
2023-02-26 22:12:49 +0100

    Implementation of MCTS with random rollout policy

--- module_6/search.py
+++ module_6/search.py
@@ -2,13 +2,20 @@ import copy
 from collections import deque
 import random
 
+SIMULATION_RUNS = 100
+
 class GameTree:
-    def __init__(self, game, selection_policy=None):
+    def __init__(self, game, rollout_policy=None, score_func=None):
         self.game = game
         self.states = {}
-        self.root = State(self.game, None)
+        self.root = State(self.game, last_move=None, parent=None)
+        if rollout_policy is None:
+            rollout_policy = self.rollout_policy
+        self.root.selection_policy = rollout_policy
         self.current_state = copy.deepcopy(self.root)
-        self.selection_policy = selection_policy
+        if score_func is None:
+            score_func = self._score_func
+        self.score_func = score_func
 
     @property
     def states(self):
@@ -19,34 +26,67 @@ class GameTree:
     def states(self, value):
         self._states = value
 
+    def rollout_policy(self):
+        """Default method for visited nodes
+
+        The only node considered to be "visited" is the root node. All other nodes will likely
+        stick to uniform random selection to expand.
+
+        Can be set by setting
+        """
+        return random.sample(tuple(self.game.board.empty_squares), 1)[0]
+
+    def _score_func(self, state):
+        """Default scoring function to evaluate a move
+
+        Takes a State as argument, and evaluates according to the current player in root node
+        """
+        if state.game.is_draw:
+            score = 0.5
+        else:
+            score = 1 if self.root.game.next_player == state.game.winner else 0
+        # Update all states, aside from the first one
+        for s in state.traverse():
+            s.wins += score
+
     def simulate(self, state):
         while not state.is_terminal_node:
             pos = state.selection_policy()
             state = state.transition(pos)
+        # Backpropagate...
+        self.score_func(state)
 
 
 class State:
     """A state in the game - current or possible future"""
-    def __init__(self, game, parent, selection_policy=None):
+    def __init__(self, game, last_move, parent, selection_policy=None):
         self.game = game
+        self.last_move = last_move
         self.parent = parent
         self.child_states = {}
         if selection_policy is None:
             selection_policy = self._selection_policy
         self.selection_policy = selection_policy
+        self.visits = 0
+        self.wins = 0
 
     @property
     def possible_moves(self):
         return self.game.board.empty_squares
 
+    @property
+    def score(self):
+        return 0 if not self.visits else self.wins / self.visits
+
     def transition(self, pos):
         if pos not in self.child_states:
-            new_state = State(copy.deepcopy(self.game), self)
+            new_state = State(copy.deepcopy(self.game), last_move=pos, parent=self)
             self.child_states[pos] = new_state
             # Only play the move if the state did not previously exist
             # Otherwise, we only need to transition to it and decide on the move after
             new_state.game.play_turn(new_state.game.next_player, pos)
         new_state = self.child_states[pos]
+        new_state.visits += 1
 
         return new_state
         # self.child_states[pos] = new_state
@@ -55,7 +95,7 @@ class State:
         ret = deque([])
         ret.appendleft(self)
         s = self
-        while s := s.parent is not None:
+        while (s := s.parent) is not None:
             ret.appendleft(s)
         return ret
 
@@ -72,17 +112,17 @@ class State:
         return random.sample(tuple(self.game.board.empty_squares), 1)[0]
 
 
-
 class MonteCarloMixin:
     def search(self, game):
-        tree = GameTree(game, selection_policy=None)
-        for i in range(5):
+        tree = GameTree(game, rollout_policy=None)
+        for i in range(SIMULATION_RUNS):
             pos = tree.root.selection_policy()
             state = tree.root.transition(pos)
             print('[Simulation]')
             tree.simulate(state)
             print('[End Simulation]')
-        return tree.root.game.board.empty_squares
+        return list(tree.root.child_states.values())
+        # return tree.root.game.board.empty_squares
 
     def rollout_policy(self):
         return 0
